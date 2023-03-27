18afe8ececcd19f357770808d25ba6d07bea0c13
Anirudh S Rao <anirudhsrao1@gmail.com>
2023-03-27 21:13:17 +0530

    basic_skeleton_ready

---
+++ template.py
@@ -0,0 +1,40 @@
+import os
+from pathlib import Path
+import logging
+
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]:%(message)s')
+package_name="Deep classifer"
+
+list_of_files = [".github/workflows/.gitkeep",
+                f"src/{package_name}/__init__.py",
+                f"src/{package_name}/components/__init__.py",
+                f"src/{package_name}/util/__init__.py",
+                f"src/{package_name}/config/__init__.py",
+                f"src/{package_name}/pipeline/__init__.py",
+                f"src/{package_name}/entity/__init__.py",
+                f"src/{package_name}/constants/__init__.py",
+                "configs/config.yaml",
+                "dvc.yaml",
+                "params.yaml",
+                "init_setup.sh",
+                "requirements.txt",
+                "requirements_dev.txt",
+                "setup.py",
+                "setup.cfg",
+                "pyproject.toml"
+                "tox.ini",
+                "research/trials.ipynb"
+                ]
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir,filename = os.path.split(filepath)
+    if filedir !="":
+        os.makedirs(filedir,exist_ok=True)
+
+    if (not os.path.exists(filepath)) or (os.path.getsize()==0):
+        with open(filepath,"w") as f:
+            pass
+
+    else:
+        logging.info(f"{filename} already exists")
